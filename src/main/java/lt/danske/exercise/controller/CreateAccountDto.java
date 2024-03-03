4a8bd8c56f4cf938b091f942e7f901ff58ab62cc
Ruggero Montesi <ruggero.montesi1@gmail.com>
2024-03-03 09:24:23 +0200

    code cleanup

--- src/main/java/lt/danske/exercise/controller/CreateAccountDto.java
+++ src/main/java/lt/danske/exercise/controller/CreateAccountDto.java
@@ -1,13 +1,8 @@
 package lt.danske.exercise.controller;
 
-
-import jakarta.annotation.Nonnull;
-import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
-import lombok.NonNull;
 import lombok.Value;
 import lt.danske.exercise.domain.AccountType;
-import jakarta.validation.constraints.NotBlank;
 import org.springframework.validation.annotation.Validated;
 
 @Builder
