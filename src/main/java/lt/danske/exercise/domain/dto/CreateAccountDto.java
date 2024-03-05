36df058391286f2c1e46a80af1160a2eae72e5eb
Ruggero Montesi <ruggero.montesi1@gmail.com>
2024-03-05 05:48:01 +0200

    beautify swagger ui details

--- src/main/java/lt/danske/exercise/domain/dto/CreateAccountDto.java
+++ src/main/java/lt/danske/exercise/domain/dto/CreateAccountDto.java
@@ -1,17 +1,20 @@
 package lt.danske.exercise.domain.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Value;
-import lt.danske.exercise.domain.Currency;
 import lt.danske.exercise.domain.AccountType;
+import lt.danske.exercise.domain.Currency;
 import org.springframework.validation.annotation.Validated;
 
 @Builder
 @Value
 @Validated
 public class CreateAccountDto {
-    long userId;
-
+    @NotNull
+    Long userId;
+    @NotNull
     AccountType accountType;
+    @NotNull
     Currency currency;
 }
