a7ae82a52f86553bae0ae2e4c5bf08e0aa7e6ef9
Ruggero Montesi <ruggero.montesi1@gmail.com>
2024-03-06 14:55:55 +0200

    remove unnecessary annotation

--- src/main/java/lt/danske/exercise/domain/dto/CreateAccountRequest.java
+++ src/main/java/lt/danske/exercise/domain/dto/CreateAccountRequest.java
@@ -2,13 +2,13 @@ package lt.danske.exercise.domain.dto;
 
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
-import lombok.Value;
-import lt.danske.exercise.domain.entity.AccountType;
+import lombok.Getter;
 import lt.danske.exercise.domain.Currency;
+import lt.danske.exercise.domain.entity.AccountType;
 import org.springframework.validation.annotation.Validated;
 
 @Builder
-@Value
+@Getter
 @Validated
 public class CreateAccountRequest {
     @NotNull
